2b4544f9dae7c6398ea69fae00e17b5cf2567bae
André devkcud Albanese <patommmmm@proton.me>
2023-10-11 03:03:52 -0300

    fix: Change `price` to `pricePerUnit`

--- api/internal/model/item.go
+++ api/internal/model/item.go
@@ -3,10 +3,10 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Item struct {
-	ID          primitive.ObjectID `bson:"_id" json:"-"`
-	Name        string             `bson:"name" json:"name"`
-	Description string             `bson:"description" json:"description"`
-	Price       float64            `bson:"price" json:"price"`
-	Quantity    int                `bson:"quantity" json:"quantity"`
-	Tags        []string           `bson:"tags" json:"tags"`
+	ID           primitive.ObjectID `bson:"_id" json:"-"`
+	Name         string             `bson:"name" json:"name"`
+	Description  string             `bson:"description" json:"description"`
+	PricePerUnit float64            `bson:"pricePerUnit" json:"pricePerUnit"`
+	Quantity     int                `bson:"quantity" json:"quantity"`
+	Tags         []string           `bson:"tags" json:"tags"`
 }
