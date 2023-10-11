be59d3cc2f9696f54c22d8f15314184d35f626eb
André devkcud Albanese <patommmmm@proton.me>
2023-10-11 00:00:43 -0300

    feat: Add gin server

--- api/cmd/storage-api/main.go
+++ api/cmd/storage-api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+const API_PORT = 8080
 
 func main() {
-    fmt.Println("Hello there!")
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+
+	if err := router.Run(fmt.Sprintf(":%d", API_PORT)); err != nil {
+		log.Fatal(err)
+	}
 }
