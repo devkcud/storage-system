e05f4b368c6f404ed8ee888ca9154c7a923d7b09
André Luis <andrescalisejr@gmail.com>
2023-10-19 16:17:58 -0300

    feat: Add search by title, description and tags

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,37 +2,77 @@
     import '$lib/styles/home.scss';
 
     import { onMount } from 'svelte';
-    import { writable } from 'svelte/store';
 
     // @ts-ignore
     import FaTrash from 'svelte-icons/fa/FaTrash.svelte';
     // @ts-ignore
+    import FaSearch from 'svelte-icons/fa/FaSearch.svelte';
+    // @ts-ignore
     import FaPen from 'svelte-icons/fa/FaPen.svelte';
 
-    let items: {
+    interface Item {
         id: string;
         name: string;
         description: string;
         pricePerUnit: number;
         quantity: number;
-        tags: string[];
-    }[];
+        tags: string[] | null;
+    }
 
+    let items: Item[] = [];
+    let filteredItems: Item[] = [];
     let loading = true;
+    let searchQuery = '';
 
     onMount(async () => {
-        const response = await fetch('http://localhost:8080/', { method: 'GET' });
-        items = await response.json();
+        try {
+            const response = await fetch('http://localhost:8080/', { method: 'GET' });
+            items = await response.json();
+            filteredItems = items;
+        } catch (error) {
+            console.error('Failed to fetch items:', error);
+        } finally {
+            loading = false;
+        }
 
-        loading = false;
+        filteredItems = items;
     });
+
+    function filterItems() {
+        if (searchQuery === '') {
+            filteredItems = items;
+        } else {
+            filteredItems = items.filter((item) => {
+                const nameMatch = item.name.toLowerCase().includes(searchQuery.toLowerCase());
+                const descriptionMatch = item.description
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase());
+                const tagMatch =
+                    item.tags &&
+                    item.tags.some((tag) => tag.toLowerCase().includes(searchQuery.toLowerCase()));
+                return nameMatch || descriptionMatch || tagMatch;
+            });
+        }
+    }
 </script>
 
 <h1 id="title">Products</h1>
 
-{#if items}
+<div id="search">
+    <FaSearch />
+
+    <input
+        type="text"
+        id="search"
+        bind:value={searchQuery}
+        placeholder="Search name, description or tag"
+        on:input={filterItems}
+    />
+</div>
+
+{#if filteredItems.length > 0}
     <div id="products">
-        {#each items as item}
+        {#each filteredItems as item}
             <div class="card">
                 <header>
                     <h2>{item.name}</h2>
@@ -69,5 +109,5 @@
 {:else if loading}
     <p class="center dimmed">Loading items...</p>
 {:else}
-    <p class="center dimmed">Ain't nobody here but us chickens (no items found)</p>
+    <p class="center dimmed">No items found</p>
 {/if}
