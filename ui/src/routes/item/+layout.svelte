770bee80a6bbb199306fc061ea548fae9fb04d14
André Luis <andrescalisejr@gmail.com>
2023-10-20 15:09:39 -0300

    feat(item): Add base page and layout

---
+++ ui/src/routes/item/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '$lib/styles/item.scss';
+
+    import { goto } from '$app/navigation';
+
+    // @ts-ignore
+    import FaArrowLeft from 'svelte-icons/fa/FaArrowLeft.svelte';
+</script>
+
+<button id="go-back" on:click={() => goto('/')}>
+    <FaArrowLeft />
+</button>
+
+<main id="item-holder">
+    <slot />
+</main>
